b3d52b5a9ded0ba3e1e9f42b83d29452a69ff503
alexmuhdot <81035830+alexmuhdot@users.noreply.github.com>
2022-10-24 00:56:29 +0100

    Add files via upload

--- termuxsetup.sh
+++ termuxsetup.sh
@@ -1,18 +1,19 @@
 pkg install root-repo
 pkg install x11-repo
 
-pkg install proxychains-ng
-pkg install tor
-pkg install git
-pkg install nmap
-pkg install perl
-pkg install ruby
+apt-get update
+ apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade -y
 
-pkg install python
-pip3 install --upgrade pip
-apt update
-apt upgrade
-apt-get install full-upgrade -y
+pkg install proxychains-ng -y
+pkg install tor -y
+pkg install git -y
+pkg install nmap -y
+pkg install perl -y
+pkg install ruby -y
+
+pkg install python -y
+pip3 install --upgrade pip 
+
 
 echo "bell-character = ignore" >> .termux/termux.properties
 echo "alias c='clear'" >> ../usr/etc/bash.bashrc
@@ -21,9 +22,9 @@ echo "alias l='ls -a'" >> ../usr/etc/bash.bashrc
 
 echo "alias pc='proxychains4 -q'" >> ../usr/etc/bash.bashrc
 
-echo "alias pcip='pc curl ifconfig.me" >> ../usr/etc/bash.bashrc
+echo "alias pct='pc curl ifconfig.me'" >> ../usr/etc/bash.bashrc
 
-echo "alias ip='curl ifconfig.me'" >> ../usr/etc/bash.bashrc
+echo "alias ipt='curl ifconfig.me'" >> ../usr/etc/bash.bashrc
 
 sed -i '18 s/./#&/' ../usr/etc/proxychains.conf
 
